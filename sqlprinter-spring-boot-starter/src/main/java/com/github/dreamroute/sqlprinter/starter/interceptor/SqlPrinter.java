30480e515c267c6eaa82d5127dd5ff2d57c0d602
w.dehai <342252328@qq.com>
2021-03-03 14:10:57 +0800

    feat: 改变sql打印样式

--- sqlprinter-spring-boot-starter/src/main/java/com/github/dreamroute/sqlprinter/starter/interceptor/SqlPrinter.java
+++ sqlprinter-spring-boot-starter/src/main/java/com/github/dreamroute/sqlprinter/starter/interceptor/SqlPrinter.java
@@ -153,7 +153,7 @@ public class SqlPrinter implements Interceptor {
                 } catch (JSQLParserException e) {
                     throw new RuntimeException("你的SQL语句语法有错误, SQL: " + result);
                 }
-                log.info("{}", "\r\n===SQL===>[" + mappedStatement.getId() + "]\r\n===SQL===>[" + result + "]");
+                log.info("{}", "\r\n===方法===>[" + mappedStatement.getId() + "]\r\n===SQL===>[" + result + "]");
             }
 
         }

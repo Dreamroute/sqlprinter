3797b8e4108a4e1b1ee073da8c14c0fa749b8edc
wangdehai <wangdehai@yzw.cn>
2023-02-03 11:01:58 +0800

    refactor: 重构控制台日志抓取工具，提取成工具类

--- sqlprinter-samples/spring-boot/src/test/java/com/github/dreamroute/sqlprinter/boot/mapper/UserMapperTest.java
+++ sqlprinter-samples/spring-boot/src/test/java/com/github/dreamroute/sqlprinter/boot/mapper/UserMapperTest.java
@@ -1,7 +1,6 @@
 package com.github.dreamroute.sqlprinter.boot.mapper;
 
-import ch.qos.logback.classic.spi.ILoggingEvent;
-import ch.qos.logback.core.read.ListAppender;
+import com.github.dreamroute.common.util.test.Appender;
 import com.github.dreamroute.sqlprinter.boot.domain.User;
 import com.github.dreamroute.sqlprinter.starter.anno.EnableSQLPrinter;
 import com.github.dreamroute.sqlprinter.starter.converter.def.DateConverter;
@@ -16,18 +15,13 @@ import org.apache.ibatis.reflection.SystemMetaObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.HashSet;
-import java.util.List;
 
 import static com.github.dreamroute.sqlprinter.boot.domain.User.Gender.MALE;
-import static com.github.dreamroute.sqlprinter.boot.mapper.AppenderUtil.create;
-import static com.github.dreamroute.sqlprinter.boot.mapper.AppenderUtil.getMessage;
 import static com.google.common.collect.Lists.newArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -59,10 +53,8 @@ class UserMapperTest {
 
     @Test
     void selectByIdTest() {
-        ListAppender<ILoggingEvent> appender = create(SqlPrinter.class);
         User user = userMapper.selectById(1L);
         assertEquals("w.dehai", user.getName());
-        assertEquals(0, appender.list.size());
     }
 
     @Test
@@ -73,46 +65,46 @@ class UserMapperTest {
         String time = "2020-01-01 01:01:10.111";
         user.setBirthday(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").parse(time));
         user.setGender(MALE);
-        ListAppender<ILoggingEvent> appender = create(SqlPrinter.class);
+        Appender appender = new Appender(SqlPrinter.class);
         userMapper.insert(user);
         assertNotNull(user.getId());
         String sql = "insert into smart_user(birthday,password,gender,name,version) VALUES ('" + time + "','123456',1,'Jaedong',null)";
-        assertTrue(getMessage(appender, 0).contains(sql));
+        assertTrue(appender.contains(sql));
     }
 
     @Test
     void updateTest() {
         User user = userMapper.selectById(1L);
         user.setPassword("update");
-        ListAppender<ILoggingEvent> appender = create(SqlPrinter.class);
+        Appender appender = new Appender(SqlPrinter.class);
         userMapper.updateById(user);
         userMapper.updateByIdExcludeNull(user);
-        assertTrue(getMessage(appender, 0).contains("update smart_user set birthday = null,password = 'update',gender = null,name = 'w.dehai',version = 0 where id = 1"));
-        assertTrue(getMessage(appender, 1).contains("update smart_user set  password = 'update',name = 'w.dehai',version = 0  where id = 1"));
+        assertTrue(appender.contains("update smart_user set birthday = null,password = 'update',gender = null,name = 'w.dehai',version = 0 where id = 1"));
+        assertTrue(appender.contains(1, "update smart_user set  password = 'update',name = 'w.dehai',version = 0  where id = 1"));
     }
 
     @Test
     void selectUsersTest() {
-        ListAppender<ILoggingEvent> appender = create(SqlPrinter.class);
+        Appender appender = new Appender(SqlPrinter.class);
         userMapper.selectUsers();
         String sql = "SELECT\n" +
                 "            *\n" +
                 "        FROM\n" +
                 "            smart_user";
-        assertTrue(getMessage(appender, 0).contains(sql));
+        assertTrue(appender.contains(sql));
     }
 
     @Test
     void selectUserByIdsTest() {
-        ListAppender<ILoggingEvent> appender = create(SqlPrinter.class);
-        List<User> users = userMapper.selectUserByIds(newArrayList(1L, 2L));
+        Appender appender = new Appender(SqlPrinter.class);
+        userMapper.selectUserByIds(newArrayList(1L, 2L));
         String sql = "select * from smart_user where id in\n" +
                 "         (  \n" +
                 "            (1)\n" +
                 "         , \n" +
                 "            (2)\n" +
                 "         )";
-        assertTrue(getMessage(appender, 0).contains(sql));
+        assertTrue(appender.contains(sql));
     }
 
     /**
@@ -122,7 +114,10 @@ class UserMapperTest {
     void filterNullTest() {
         MetaObject mo = SystemMetaObject.forObject(sqlPrinter);
         mo.setValue("filter", new HashSet<>());
+        Appender appender = new Appender(SqlPrinter.class);
         userMapper.selectById(1L, "id", "name");
+        String sql = "select id, name from smart_user where id = 1";
+        assertTrue(appender.contains(sql));
     }
 
 }
--- sqlprinter-samples/spring-boot/src/test/java/com/github/dreamroute/sqlprinter/boot/mapper/UserMapperWithFormatTest.java
+++ sqlprinter-samples/spring-boot/src/test/java/com/github/dreamroute/sqlprinter/boot/mapper/UserMapperWithFormatTest.java
@@ -1,7 +1,6 @@
 package com.github.dreamroute.sqlprinter.boot.mapper;
 
-import ch.qos.logback.classic.spi.ILoggingEvent;
-import ch.qos.logback.core.read.ListAppender;
+import com.github.dreamroute.common.util.test.Appender;
 import com.github.dreamroute.sqlprinter.boot.domain.User;
 import com.github.dreamroute.sqlprinter.starter.anno.EnableSQLPrinter;
 import com.github.dreamroute.sqlprinter.starter.anno.SqlprinterProperties;
@@ -20,11 +19,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import javax.annotation.Resource;
 import javax.sql.DataSource;
 import java.text.SimpleDateFormat;
-import java.util.List;
 
 import static com.github.dreamroute.sqlprinter.boot.domain.User.Gender.MALE;
-import static com.github.dreamroute.sqlprinter.boot.mapper.AppenderUtil.create;
-import static com.github.dreamroute.sqlprinter.boot.mapper.AppenderUtil.getMessage;
 import static com.google.common.collect.Lists.newArrayList;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -73,38 +69,38 @@ class UserMapperWithFormatTest {
         String time = "2020-01-01 01:01:10.111";
         user.setBirthday(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").parse(time));
         user.setGender(MALE);
-        ListAppender<ILoggingEvent> appender = create(SqlPrinter.class);
+        Appender appender = new Appender(SqlPrinter.class);
         userMapper.insert(user);
         assertNotNull(user.getId());
         String sql = "INSERT INTO smart_user (birthday, password, gender, name, version) VALUES ('2020-01-01 01:01:10.111', '123456', 1, 'Jaedong', NULL)";
-        assertTrue(getMessage(appender, 0).contains(sql));
+        assertTrue(appender.contains(sql));
     }
 
     @Test
     void updateTest() {
         User user = userMapper.selectById(1L);
         user.setPassword("update");
-        ListAppender<ILoggingEvent> appender = create(SqlPrinter.class);
+        Appender appender = new Appender(SqlPrinter.class);
         userMapper.updateById(user);
         userMapper.updateByIdExcludeNull(user);
-        assertTrue(getMessage(appender, 0).contains("UPDATE smart_user SET birthday = NULL, password = 'update', gender = NULL, name = 'w.dehai', version = 0 WHERE id = 1"));
-        assertTrue(getMessage(appender, 1).contains("UPDATE smart_user SET password = 'update', name = 'w.dehai', version = 0 WHERE id = 1"));
+        assertTrue(appender.contains("UPDATE smart_user SET birthday = NULL, password = 'update', gender = NULL, name = 'w.dehai', version = 0 WHERE id = 1"));
+        assertTrue(appender.contains(1, "UPDATE smart_user SET password = 'update', name = 'w.dehai', version = 0 WHERE id = 1"));
     }
 
     @Test
     void selectUsersTest() {
-        ListAppender<ILoggingEvent> appender = create(SqlPrinter.class);
+        Appender appender = new Appender(SqlPrinter.class);
         userMapper.selectUsers();
         String sql = "SELECT * FROM smart_user";
-        assertTrue(getMessage(appender, 0).contains(sql));
+        assertTrue(appender.contains(sql));
     }
 
     @Test
     void selectUserByIdsTest() {
-        ListAppender<ILoggingEvent> appender = create(SqlPrinter.class);
-        List<User> users = userMapper.selectUserByIds(newArrayList(1L, 2L));
+        Appender appender = new Appender(SqlPrinter.class);
+        userMapper.selectUserByIds(newArrayList(1L, 2L));
         String sql = "SELECT * FROM smart_user WHERE id IN ((1), (2))";
-        assertTrue(getMessage(appender, 0).contains(sql));
+        assertTrue(appender.contains(sql));
     }
 
 }
